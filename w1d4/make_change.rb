013b1cc4b25f131d5da1b9de9f9392745aea7d58
hoang6 <ho4ngt@gmail.com>
2015-02-20 06:31:36 -0800

    complete make_change.rb

--- w1d4/make_change.rb
+++ w1d4/make_change.rb
@@ -1,32 +1,34 @@
-# def make_change(num, coins)
-#   left_coins = coins.select { |coin| coin <= num }
-#   return nil if left_coins.count == 0
-#
-#   p chosen_coin = left_coins[0]
-#   if
-#
-#   elsif chosen_coin == num
-#     [chosen_coin]
-#   else
-#     make_change(num, coins) << make_change(num - chosen_coin, left_coins)
-#   end
-# end
+def make_change(num, max, coins)
+  return nil if coins.count == 0
 
-
-def make_change(num, coins)
-  return nil if left_coins.count == 0
-
-  chosen_coin = coins[0]
-  if chosen_coin > num
-    coins.drop(1)
-  elsif chosen_coin == num
-    [chosen_coin]
+  if coins.first == num || num == 0
+    [coins.first]
   else
-    exchanges = 
-    left_coins = coins.select { |coin| coin <= num }
-    make_change(num - chosen_coin, left_coins)
+    left_coins = coins.select { |coin| (coin <= num) && (coin <= max) }
+    # p "#{left_coins} #{num}"
+      make_change(num - left_coins.first, max, left_coins) << left_coins.first
   end
 end
 
 
-p make_change(14, [10, 7, 1])
+def fewer_coins(num, coins)
+  coin_num = make_change(num, coins[0], coins).count
+  chosen_coin = coins[0]
+
+  coins.drop(1).each do |coin|
+    if make_change(num, coin, coins).count < coin_num
+      coin_num = make_change(num, coin, coins).count
+      chosen_coin = coin
+    end
+  end
+
+  puts "Biggest coin: #{chosen_coin}"
+  make_change(num, chosen_coin, coins)
+end
+
+# p make_change(234, 50, [100, 50, 20, 10, 5, 1])
+# p fewer_coins(267, [100, 50, 20, 10, 5, 1])
+
+p fewer_coins(14, [10, 7, 1])
+
+# p make_change(23, 7, [10, 7, 1])
